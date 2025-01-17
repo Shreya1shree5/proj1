f1778ab2b2fa03ccb8915ab62fe525736afdba16
Shreya1shree5 <47668807+Shreya1shree5@users.noreply.github.com>
2025-01-17 22:38:11 +0530

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -9,36 +9,23 @@ output "gke_cluster_endpoint" {
   value       = google_container_cluster.primary.endpoint
 }
 
-# Compute Instance outputs
-output "compute_instance_public_ip" {
-  description = "The public IP of the Ubuntu compute instance"
-  value       = google_compute_instance.ubuntu_instance.network_interface[0].access_config[0].nat_ip
+output "gke_cluster_zone" {
+  description = "The zone of the GKE cluster"
+  value       = google_container_cluster.primary.location
 }
 
-output "compute_instance_private_ip" {
-  description = "The private IP of the Ubuntu compute instance"
-  value       = google_compute_instance.ubuntu_instance.network_interface[0].network_ip
+output "gke_cluster_master_version" {
+  description = "The master version of the GKE cluster"
+  value       = google_container_cluster.primary.master_version
 }
 
-# Network outputs
-output "vpc_network_name" {
-  description = "The name of the VPC network"
-  value       = google_compute_network.main.name
+# Node Pool outputs
+output "gke_node_pool_name" {
+  description = "The name of the GKE node pool"
+  value       = google_container_node_pool.primary_nodes.name
 }
 
-output "vpc_network_id" {
-  description = "The ID of the VPC network"
-  value       = google_compute_network.main.id
-}
-
-# Subnet outputs
-output "subnet_names" {
-  description = "The names of the subnets"
-  value       = google_compute_subnetwork.main[*].name
-}
-
-# Artifact Registry output
-output "artifact_registry_repository" {
-  description = "The name of the Artifact Registry repository"
-  value       = google_artifact_registry_repository.app_repo.name
+output "gke_node_pool_instance_group_urls" {
+  description = "The instance group URLs of the GKE node pool"
+  value       = google_container_node_pool.primary_nodes.instance_group_urls
 }
